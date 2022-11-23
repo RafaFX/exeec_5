891b14becd95313fb60320f608b18c65e0b3f7d3
Rafael Lima da Silva <78000302+RafaFX@users.noreply.github.com>
2022-11-23 19:10:59 -0300

    Add files via upload

---
+++ Exercícios V/src/Frame.java
@@ -0,0 +1,49 @@
+
+public class Frame {
+	private  int id;
+	private int tempoCarga;
+	private int qtdRef;
+	private int tempoRef;
+	private int br;
+	private int bm;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public int getTempoCarga() {
+		return tempoCarga;
+	}
+	public void setTempoCarga(int tempoCarga) {
+		this.tempoCarga = tempoCarga;
+	}
+	public int getQtdRef() {
+		return qtdRef;
+	}
+	public void setQtdRef(int qtdRef) {
+		this.qtdRef = qtdRef;
+	}
+	public int getTempoRef() {
+		return tempoRef;
+	}
+	public void setTempoRef(int tempoRef) {
+		this.tempoRef = tempoRef;
+	}
+	public int getBr() {
+		return br;
+	}
+	public void setBr(int br) {
+		this.br = br;
+	}
+	public int getBm() {
+		return bm;
+	}
+	public void setBm(int bm) {
+		this.bm = bm;
+	}
+	
+	
+	
+}
